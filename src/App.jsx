9e2143df5331c5ca354efc2b319980a598c4a782
Providential Synnah <57027054+PROVISYNNAHGITHUB@users.noreply.github.com>
2026-03-14 05:54:36 +0530

    ready for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import Home from './pages/Home'
-import Archive from './pages/Archive'
-import Admin from './pages/Admin'
-import Login from './pages/Login'
-import Footer from './components/Footer'
+  import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+  import Home from './pages/Home'
+  import Archive from './pages/Archive'
+  import Admin from './pages/Admin'
+  import Login from './pages/Login'
+  import Footer from './components/Footer'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <nav>
-        <div className="container">
-          <Link to="/" className="logo-link">
-        <img src="/assets/kjcf-Logo.png" alt="AIKJCF" className="logo" />
-        <h1 className="org-name">All India Khasi Jaintia Christian Fellowship</h1>
-          </Link>
-          <Link to="/">Home</Link>
-          <Link to="/archive">Archive</Link>
-          <Link to="/admin">Admin</Link>
+  function App() {
+    return (
+      <BrowserRouter>
+        <nav>
+          <div className="container">
+            <Link to="/" className="logo-link">
+          <img src="/assets/kjcf-Logo.png" alt="AIKJCF" className="logo" />
+          <h1 className="org-name">All India Khasi Jaintia Christian Fellowship</h1>
+            </Link>
+            <Link to="/">Home</Link>
+            <Link to="/archive">Archive</Link>
+            <Link to="/admin">Admin</Link>
+          </div>
+        </nav>
+        <div className='overview-section'>
+        <h1>Overview</h1>
+        <p id='overview'>The All India Khasi Jaintia Fellowship (AIKJCF)</p>
         </div>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/archive" element={<Archive />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/login" element={<Login />} />
-      </Routes> 
-      <footer>
-        <Footer />
-      </footer>
-    </BrowserRouter>
-  )
-}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/archive" element={<Archive />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/login" element={<Login />} />
+        </Routes> 
+        <footer>
+          <Footer />
+        </footer>
+      </BrowserRouter>
+    )
+  }
 
-export default App
+  export default App
